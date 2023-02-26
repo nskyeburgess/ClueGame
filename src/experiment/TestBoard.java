a66742127bc6db11f2cf8123f5f0d54f395e3348
nskyeburgess <nburgess@mines.edu>
2023-02-26 14:46:15 -0700

    first commit

---
+++ src/experiment/TestBoard.java
@@ -0,0 +1,17 @@
+package experiment;
+
+public class TestBoard {
+public TestBoard() {
+		
+	}
+	void calcTargets( TestBoardCell startCell, int pathlength) {
+	}
+	TestBoardCell getCell(int row, int col) {
+		return null;
+	}
+	TestBoardCell.getTargets() {
+		return null;
+	}
+}
+
+}
---
+++ src/experiment/TestBoardCell.java
@@ -0,0 +1,19 @@
+package experiment;
+
+public class TestBoardCell {
+	public TestBoardCell(int SingleCell) {
+		int row;
+		int column;
+	}
+	 void addAdjacency(TestBoardCell.SingleCell);
+	 
+	 void setRoom(boolean);
+	 
+	 boolean isRoom();
+	 
+	 void setOccupied(boolean);
+	 
+	 boolean getOccupied();
+}
+
+}
