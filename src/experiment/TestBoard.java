7e8327781cf92f3386ea191f839f8a704e13375a
threetwothree <ipacheco@mines.edu>
2023-02-27 17:20:08 -0700

    Added to TestBoard

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,17 +1,63 @@
 package experiment;
 
+
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class TestBoard {
-public TestBoard() {
+	private Set<TestBoardCell> visited = new HashSet<TestBoardCell>();
+	private Set<TestBoardCell> targets = new HashSet<TestBoardCell>();
+	// empty constructor
+	public TestBoard() {
 		
 	}
-	void calcTargets( TestBoardCell startCell, int pathlength) {
+	
+	// calcs legal targets for a move from startCell of length pathlength
+	public void calcTargets( TestBoardCell startCell, int pathlength) {
+		//Set<TestBoardCell> visited = new HashSet<TestBoardCell>();
+		//Set<TestBoardCell> targets = new HashSet<TestBoardCell>();
+		visited.add(startCell);
+		findAllTargets(startCell,pathlength);
+		
+		
 	}
+	
+	
+	// recursive function for calcTargets
+	public void findAllTargets(TestBoardCell startCell, int pathlength) {
+		Set<TestBoardCell> adjCells = getAdjList(startCell);
+		//loop through adjCells
+		for(TestBoardCell adjC : adjCells) {
+			if(visited.contains(adjC)) {
+				continue;
+			}
+			visited.add(adjC);
+			if(pathlength == 1) {
+				targets.add(adjC);
+			}
+			else {
+				findAllTargets(adjC, pathlength -1);
+			}
+			visited.remove(adjC);
+			
+		}
+	}
+	
+	// returns the cell from the board at row,col
 	TestBoardCell getCell(int row, int col) {
+		TestBoardCell cell = null;
+		// set cell to row,col
+		
+		return cell;
+	}
+	
+	// gets targets created by calcTargets()
+	Set<TestBoardCell> getTargets() {
+		
 		return null;
 	}
-	TestBoardCell.getTargets() {
-		return null;
-	}
-}
+
 
 }
--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,11 +1,27 @@
 package experiment;
 
+import java.util.Map;
+import java.util.Set;
+
 public class TestBoardCell {
+	
+	//constructor
+	
 	public TestBoardCell(int SingleCell) {
 		int row;
 		int column;
 	}
+
+	
 	 void addAdjacency(TestBoardCell.SingleCell);
+	 
+	 // returns the adjacency list for the cell
+	 public Set<TestBoardCell> getAdjList(TestBoardCell cell){
+		 Set<TestBoardCell> adjSet = new HashSet<TestBoardCell>();
+		 
+		 
+		 return adjSet;
+	 }
 	 
 	 void setRoom(boolean);
 	 
@@ -16,4 +32,4 @@ public class TestBoardCell {
 	 boolean getOccupied();
 }
 
-}
+
